3aaf01ce2f233be0293214937db9c1cddb146281
Gavin Newcomer <gjnprivate@gmail.com>
2026-07-21 08:25:43 -0700

    fix: rescope push co owner

--- packages/email-builder/src/Reader/core.tsx
+++ packages/email-builder/src/Reader/core.tsx
@@ -1,19 +1,19 @@
 import React, { createContext, useContext } from 'react';
 import { z } from 'zod';
 
-import { Avatar, AvatarPropsSchema } from '@usewaypoint/block-avatar';
-import { Button, ButtonPropsSchema } from '@usewaypoint/block-button';
-import { Divider, DividerPropsSchema } from '@usewaypoint/block-divider';
-import { Heading, HeadingPropsSchema } from '@usewaypoint/block-heading';
-import { Html, HtmlPropsSchema } from '@usewaypoint/block-html';
-import { Image, ImagePropsSchema } from '@usewaypoint/block-image';
-import { Spacer, SpacerPropsSchema } from '@usewaypoint/block-spacer';
-import { Text, TextPropsSchema } from '@usewaypoint/block-text';
+import { Avatar, AvatarPropsSchema } from '@push-co/block-avatar';
+import { Button, ButtonPropsSchema } from '@push-co/block-button';
+import { Divider, DividerPropsSchema } from '@push-co/block-divider';
+import { Heading, HeadingPropsSchema } from '@push-co/block-heading';
+import { Html, HtmlPropsSchema } from '@push-co/block-html';
+import { Image, ImagePropsSchema } from '@push-co/block-image';
+import { Spacer, SpacerPropsSchema } from '@push-co/block-spacer';
+import { Text, TextPropsSchema } from '@push-co/block-text';
 import {
   buildBlockComponent,
   buildBlockConfigurationDictionary,
   buildBlockConfigurationSchema,
-} from '@usewaypoint/document-core';
+} from '@push-co/document-core';
 
 import ColumnsContainerPropsSchema from '../blocks/ColumnsContainer/ColumnsContainerPropsSchema';
 import ColumnsContainerReader from '../blocks/ColumnsContainer/ColumnsContainerReader';
--- packages/email-builder/src/blocks/Container/ContainerReader.tsx
+++ packages/email-builder/src/blocks/Container/ContainerReader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container as BaseContainer } from '@usewaypoint/block-container';
+import { Container as BaseContainer } from '@push-co/block-container';
 
 import { ReaderBlock } from '../../Reader/core';
 
